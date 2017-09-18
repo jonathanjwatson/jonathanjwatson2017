df508d74088b8b94618d31bdbb505a7c8aab6405
jwats287 <jwats287@users.noreply.github.com>
2017-09-18 14:59:43 -0400

    Updated server again

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ connection.on('error', (err) => {
 }); 
 
 app.use(bodyParser.json());
-app.get('/', (req,res) => {
-  res.send('Hello world!')
-})
+
+app.use('/api/project', ProjectsController);
+// app.use('/sayHello', NodemailerController);
 app.use(express.static(__dirname + '/client/build/'));
-app.get('/', (req,res) => {
-  res.sendFile(__dirname + '/client/build/index.html')
-})
+app.get('*', (req,res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+  })
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
