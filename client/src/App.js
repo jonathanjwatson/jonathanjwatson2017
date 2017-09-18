ba53c55c8dbaa1c425ef89bc2726dca67dd1f097
jwats287 <jwats287@users.noreply.github.com>
2017-09-18 15:25:55 -0400

    Yarn add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+      <div>
+      <Router>
+        <div>
+        <Route exact path="/" component={Home} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      </Router>
+      </div>
       </div>
     );
   }
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                Welcome to Jonathan J. Watson's Portfolio
+            </div>
+        );
+    }
+}
+
+export default Home;
